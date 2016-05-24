732dd310273b8aaa7324d515383db01388575d76
Hope Foster-Reyes <hopealso@gmail.com>
2016-05-24 18:59:28 -0500

    Test conflicts with concurrent changes.

--- Test.R
+++ Test.R
@@ -3,3 +3,5 @@
 # More testing
 
 # GIAN - Cloned repo on my laptop, opened file, typing in notes directly on my R Studio. Will save file locally then try to committ changes.
+
+# HFR - These changes should bump into Gian's.
