0a5ddfaef2b58834aadcc329d58a15a72b21df87
Hope Foster-Reyes <hopealso@gmail.com>
2016-05-24 19:39:42 -0500

    Remove merge artifacts.

--- Test.R
+++ Test.R
@@ -4,9 +4,5 @@
 
 # GIAN - Cloned repo on my laptop, opened file, typing in notes directly on my R Studio. Will save file locally then try to committ changes.
 
-<<<<<<< HEAD
 # HFR - These changes should bump into Gian's.
-=======
 ### Changes by Gian ________ ###
-
->>>>>>> 11c3221aed8131f7be96d1864603e3eb29f7e469
