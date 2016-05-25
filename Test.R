11c3221aed8131f7be96d1864603e3eb29f7e469
Giancarlo DIBonaventi <grd@uchicago.edu>
2016-05-24 19:25:25 -0500

    Gian Try to commit changes again
    7:25 pm

--- Test.R
+++ Test.R
@@ -4,5 +4,5 @@
 
 # GIAN - Cloned repo on my laptop, opened file, typing in notes directly on my R Studio. Will save file locally then try to committ changes.
 
-### Changes by Gian ###
+### Changes by Gian ________ ###
 
