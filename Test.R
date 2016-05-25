0734ebe815eb399a30c9ae8ee79c707237d9cbdb
Giancarlo DIBonaventi <grd@uchicago.edu>
2016-05-24 19:02:37 -0500

    Gian commit changes before Hope
    Added ### line of comment

--- Test.R
+++ Test.R
@@ -3,3 +3,6 @@
 # More testing
 
 # GIAN - Cloned repo on my laptop, opened file, typing in notes directly on my R Studio. Will save file locally then try to committ changes.
+
+### Changes by Gian ###
+
